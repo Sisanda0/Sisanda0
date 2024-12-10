2d1f98fe14c2446e240dc330070c78f2488d364c
sisanda0 <s.manqina@calivision.tech>
2024-12-10 05:49:27 +0200

    Plans content

--- Downloads/FitClub-Starter-d82a075a4e478e1ece2ecbc6aae57d7eeb496dbf/src/components/Plans/Plans.jsx
+++ Downloads/FitClub-Starter-d82a075a4e478e1ece2ecbc6aae57d7eeb496dbf/src/components/Plans/Plans.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import './Plans.css'
+import React from 'react';
+import { plansData } from '../../data/plansData';
+import whiteTick from '../../assets/whiteTick.png';
+import './Plans.css';
 const Plans =()=>{
     return (
         <div className="plans-container">
@@ -8,8 +10,32 @@ const Plans =()=>{
                 <span>YOUR JOURNEY</span>
                 <span className="stroke-text">NOW WITH US</span>
             </div>
-        </div>
-    )
-}
 
-export default Plans
+
+            {/* plans card */}
+            <div className="plans">
+                {plansData.map((plan,i)=>(
+                    <div clasName="plan" key={i}>
+                        {plan.icon}
+                        <span>{plan.name}</span>
+                        <span>$ {plan.price}</span>
+
+                        <div className="features">
+                            {plan.features.map((feature,i)=>(
+                                <div className="feature">
+                                    <img src={whiteTick} alt=""/>
+                                    <span key={i}>{feature}</span>
+                                </div>
+                            ))}
+                            </div>
+                        </div>
+                ))}
+
+            </div>
+
+            
+        </div>
+    );
+};
+
+export default Plans;
