35bf3bd65095e998b411641837f2e6614f3c3c75
sisanda0 <s.manqina@calivision.tech>
2024-12-10 20:33:09 +0200

    Added testimonials heading

--- Downloads/FitClub-Starter-d82a075a4e478e1ece2ecbc6aae57d7eeb496dbf/src/assets/Testimonials/Testimonials.jsx
+++ Downloads/FitClub-Starter-d82a075a4e478e1ece2ecbc6aae57d7eeb496dbf/src/assets/Testimonials/Testimonials.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 import './Testimonials.css'
 const Testimonials = ()=>{
     return (
-        <div>Testimonials</div>
+        <div className="Testimonials">
+            <div className="left-t">
+               <span className="stroke-text">Testimonials</span>
+               <span>What they </span>
+               <span className="stroke-text">say about us</span> 
+            </div>
+            <div className="right-t"></div>
+
+
+        </div>
+            
     )
 }
 
