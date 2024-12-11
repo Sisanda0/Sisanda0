03fb17dc3df98c5fda4b787f097e2d7dee2a04b1
sisanda0 <s.manqina@calivision.tech>
2024-12-11 17:10:46 +0200

    Done with Join

--- Downloads/FitClub-Starter-d82a075a4e478e1ece2ecbc6aae57d7eeb496dbf/src/components/Join/Join.jsx
+++ Downloads/FitClub-Starter-d82a075a4e478e1ece2ecbc6aae57d7eeb496dbf/src/components/Join/Join.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
-import './Join.css'
+import React, {useRef} from 'react';
+import './Join.css';
+import emailjs from '@emailjs/browser'
 const Join =()=>{
+    const form = useRef()
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs
+          .sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, {
+            publicKey: 'YOUR_PUBLIC_KEY',
+          })
+          .then(
+            () => {
+              console.log('SUCCESS!');
+            },
+            (error) => {
+              console.log('FAILED...', error.text);
+            },
+          );
+      };
     return (
 
         <div className="Join" id = "join-us">
@@ -8,17 +27,18 @@ const Join =()=>{
                 <hr />
                 <div>
                     <span className="stroke-text">READY TO</span>
-                    <span> LEVEL UP</span>
+                    <span>LEVEL UP</span>
                 </div>
                 <div>
                     <span>YOUR BODY</span>
-                    <span className="stroke-text"> WITH US?</span>
+                    <span className="stroke-text">WITH US?</span>
                 </div>
             </div>
             <div className="right-j">
 
-                <form action="" className="email-container">
+                <form ref={form} className="email-container" onSubmit ={sendEmail}>
                     <input type="email" name="user_email" placeholder="Enter your Email address"/>
+                    <button className="btn btn-j">Join Now</button>
                 </form>
             </div>
         </div>
