d4f1abcea7b4a3dbab49cb27f32fd064944046e9
sisanda0 <s.manqina@calivision.tech>
2024-12-14 20:13:34 +0200

    Added responsiveness

--- Downloads/FitClub-Starter-d82a075a4e478e1ece2ecbc6aae57d7eeb496dbf/src/assets/Testimonials/Testimonials.jsx
+++ Downloads/FitClub-Starter-d82a075a4e478e1ece2ecbc6aae57d7eeb496dbf/src/assets/Testimonials/Testimonials.jsx
@@ -11,7 +11,7 @@ const Testimonials = ()=>{
     const tLength= testimonialsData.length;
 
     return (
-        <div className="Testimonials">
+        <div className="testimonials">
             <div className="left-t">
                <span >Testimonials</span>
                <span>What they </span>
