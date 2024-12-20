47f44d1fc2a6533b4b3af5e3d0ffebfd3212118d
sisanda0 <s.manqina@calivision.tech>
2024-12-20 13:09:59 +0200

    Color Changed

--- Downloads/FitClub-Starter-d82a075a4e478e1ece2ecbc6aae57d7eeb496dbf/src/components/Footer/Footer.jsx
+++ Downloads/FitClub-Starter-d82a075a4e478e1ece2ecbc6aae57d7eeb496dbf/src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Footer.css'
-import  Github from "../../assets/facebook.png";
+import  Facebook from "../../assets/facebook.png";
 import Instagram from  "../../assets/instagram.png";
 import LinkedIn from "../../assets/linkedin.png";
 import Logo from "../../assets/logo3_note.png";
@@ -13,8 +13,10 @@ const Footer =() =>{
         <div className="footer">
             <div className="social-links">
 
+
             
-                <img src={Github} alt="" />
+                <img src={Facebook} alt="" />
+             
                 <img src={Instagram} alt=""/>
                 <img src= {LinkedIn} alt =""/>
             </div>
